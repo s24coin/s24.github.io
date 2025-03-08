c459569f8bd1ab09b36d1c98fe8b0c41ed90f626
s24coin <ciaoqwd@gmail.com>
2025-03-09 00:34:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Funzione per scrollare alla sezione 'Why Us'
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    window.scrollTo({
+        top: section.offsetTop,
+        behavior: 'smooth'
+    });
+}
